76761b641f39e9a27b927d524871a5e29efab04d
hars321 <54657843+hars321@users.noreply.github.com>
2021-02-10 00:58:21 +0530

    ready to deploy try2

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var io=socket(server, {
   handlePreflightRequest: (req, res) => {
       const headers = {
           "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+          "Access-Control-Allow-Origin": "*", //or the specific origin you want to give access to,
           "Access-Control-Allow-Credentials": true
       };
       res.writeHead(200, headers);
