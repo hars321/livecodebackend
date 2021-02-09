ffa8bcc8e647e6dbd6d1ba6f8f78a8de9255b954
hars321 <54657843+hars321@users.noreply.github.com>
2021-02-10 00:56:13 +0530

    ready to deploy try2

--- index.js
+++ index.js
@@ -29,9 +29,15 @@ var server=app.listen(port,()=>{
 
 
 var io=socket(server, {
-    cors: {
-      origin: '*',
-    }
+  handlePreflightRequest: (req, res) => {
+      const headers = {
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+          "Access-Control-Allow-Credentials": true
+      };
+      res.writeHead(200, headers);
+      res.end();
+  }
 });
 
 
